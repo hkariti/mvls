b550eb58ee43d3f4825c41c3206b7c033265e85e
Hagai Kariti <hkariti@gmail.com>
2018-12-09 20:45:46 +0200

    reset of the file by itay

--- wet1/Ex1_301781613.m
+++ wet1/Ex1_301781613.m
@@ -2,6 +2,7 @@
 %  Itay Levi - 203192216 
 %  Hagai Kariti - 301781613
 
+        
 %% Clear all
 clear all;
 close all;
@@ -48,13 +49,13 @@ figure(2);
 subplot(2, 1, 1);
 plot(interval, signal(start_index:end_index));
 xlabel('Time [s]');
-ylabel('Amplitude');
+ylabel('Amp');
 xlim([interval(1) interval(end)]);
 hold all;
 
 subplot(2, 1, 2);
 plot(interval, signal(start_index:end_index));
-xlabel('Time [s]');
+xlabel('Time[sec]');
 ylabel('Amplitude');
 xlim([interval(1) interval(end)]);
 hold all;
@@ -70,7 +71,7 @@ sampled_interval = downsample(interval, downsample_factor);
 % plot
 subplot(2, 1, 1);
 stem(sampled_interval, sampled_signal(start_index:end_index));
-title("Continunous signal and 2Khz-sampled signal in times [2.99,3.01]s");
+title(['Original signal v.s. Reconstructions, Fs = ', num2str(sample_frequency), '[Hz]']);
 legend('Continuous [16KHz]', 'Sampled (2KHz)');
 %% Q5
 % Calculate fft on sampled signal
@@ -84,3 +85,104 @@ plot(frequencies, abs(signal_fft));
 title('FFT of 2KHz sampled signal');
 xlabel('Frequencies [Hz]');
 ylabel('Magnitude');
+%% Q7
+x_up1 = upsample(sampled_signal,downsample_factor);
+% t=t2;
+
+m_US1 = find((t>=2.99) & (t<=3.01));
+dt = t(m_US1);
+
+Int_sinc=sinc((-256:255)/downsample_factor);
+x_ID1_conv=conv(x_up1,Int_sinc);
+x_ID1=x_ID1_conv(256:(length(x_ID1_conv)-256));
+
+figure(2);
+subplot(2,1,1);
+plot(dt,x_ID1(m_US1));
+hold all;
+%% Q8
+Int_ZOH1=linspace(1,1,downsample_factor);
+x_ZOH1_conv=conv(x_up1,Int_ZOH1);
+x_ZOH1=x_ZOH1_conv(1:(length(x_ZOH1_conv)-downsample_factor+1));
+
+figure(2);
+subplot(2,1,1);
+plot(dt,x_ZOH1(m_US1));
+hold all;
+%% Q9
+Int_FOH1=conv(Int_ZOH1,Int_ZOH1)/downsample_factor;
+x_FOH1_conv=conv(x_up1,Int_FOH1);
+x_FOH1=x_FOH1_conv(downsample_factor:(length(x_FOH1_conv)-downsample_factor+1));
+
+figure(2);
+subplot(2,1,1);
+plot(dt,x_FOH1(m_US1));
+hold all;
+
+legend('original','sampled','ideal','ZOH','FOH');
+%% Q11
+Fs2=800;
+N_DS2=sample_frequency_continuous/Fs2;
+x_DS2=downsample(signal,N_DS2);
+t_DS2=downsample(t,N_DS2);
+
+m11=find((t_DS2>=2.99) & (t_DS2<=3.01));
+dx_DS2=x_DS2(m11);
+dt_DS2=t_DS2(m11);
+
+N2=4*Fs2;
+x11=fft(x_DS2);
+w2_FFT=linspace(-Fs2/2,Fs2/2,N2);
+x2_FFT = fftshift( x11 );
+
+figure(1);
+subplot(3,1,3);
+plot(w2_FFT,abs(x2_FFT));
+title(['F_s = ',num2str(Fs2), 'Hz']);
+xlabel('f[Hz]');
+ylabel('|DFT(X)|');
+hold all;
+
+
+figure(2);
+subplot(2,1,2);
+stem(dt_DS2,dx_DS2)
+title(['F_s = ',num2str(Fs2), 'Hz']);
+xlabel('Time[sec]');
+ylabel('Amp');
+hold all;
+
+x_up2 = upsample(x_DS2,N_DS2);
+t_US2 = t;
+
+m_US2=find((t_US2>=2.99) & (t_US2<=3.01));
+dt_US2=t_US2(m_US2);
+
+Int_sinc=sinc((-256:255)/N_DS2);
+x_ID2_conv=conv(x_up2,Int_sinc);
+x_ID2=x_ID2_conv(256:(length(x_ID2_conv)-256));
+
+figure(2);
+subplot(2,1,2);
+plot(dt_US2,x_ID2(m_US2));
+hold all;
+
+Int_ZOH2=linspace(1,1,N_DS2);
+x_ZOH2_conv=conv(x_up2,Int_ZOH2);
+x_ZOH2=x_ZOH2_conv(1:(length(x_ZOH2_conv)-N_DS2+1));
+
+figure(2);
+subplot(2,1,2);
+plot(dt_US2,x_ZOH2(m_US2));
+hold all;
+
+Int_FOH2=conv(Int_ZOH2,Int_ZOH2)/N_DS2;
+x_FOH2_conv=conv(x_up2,Int_FOH2);
+x_FOH2=x_FOH2_conv(N_DS2:(length(x_FOH2_conv)-N_DS2+1));
+
+figure(2);
+subplot(2,1,2);
+plot(dt_US2,x_FOH2(m_US2));
+hold all;
+
+legend('original','sampled','ideal','ZOH','FOH');
